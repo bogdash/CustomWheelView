41921e6651b3ca4e3c8e522904df00cc56dfffde
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-01-30 17:23:05 +0300

    Add rotation to custom view and add view binding

--- app/src/main/java/com/bogdash/customwheelview/WheelView.kt
+++ app/src/main/java/com/bogdash/customwheelview/WheelView.kt
@@ -1,17 +1,23 @@
 package com.bogdash.customwheelview
 
+import android.animation.ValueAnimator
+import android.annotation.SuppressLint
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
+import androidx.core.animation.doOnEnd
+import kotlin.random.Random
 
 class WheelView(context: Context, attributeSet: AttributeSet) : View(context, attributeSet) {
     private val paint = Paint()
-    private val startAngle = -180f
+    private var startAngle = 0
+    private lateinit var animator: ValueAnimator
 
-    private  val colors = listOf(
+    private val colors = listOf(
         Color.RED,
         Color.rgb(255, 127, 0), // orange
         Color.YELLOW,
@@ -51,5 +57,33 @@ class WheelView(context: Context, attributeSet: AttributeSet) : View(context, at
                 paint
             )
         }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        event?.let { it ->
+            when (it.action) {
+                MotionEvent.ACTION_UP -> {
+                    val initialAngle = startAngle % 360
+                    startAngle = Random.nextInt(360, 3600)
+                    val newDuration = (startAngle - initialAngle) / 360 * 1000L
+
+                    animator = ValueAnimator.ofFloat(initialAngle.toFloat(), startAngle.toFloat())
+                    animator.apply {
+                        duration = newDuration
+                        addUpdateListener {
+                            rotation = it.animatedValue as Float
+                        }
+                        doOnEnd {
+
+                        }
+                        start()
+                    }
+                }
+
+                else -> {}
+            }
+        }
+        return true
     }
 }
