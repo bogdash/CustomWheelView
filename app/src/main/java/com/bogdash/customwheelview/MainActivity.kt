1453fa8edd4cb530546ac9aa405ea6f94a0ba574
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-01-31 16:18:58 +0300

    Add seekbar

--- app/src/main/java/com/bogdash/customwheelview/MainActivity.kt
+++ app/src/main/java/com/bogdash/customwheelview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.bogdash.customwheelview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
 import com.bogdash.customwheelview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +16,24 @@ class MainActivity : AppCompatActivity() {
         binding.button.setOnClickListener {
             binding.wheelView.reset()
             binding.tvColor.text = getString(R.string.touch)
+            binding.seekBar.progress = 50
         }
+
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                val scale = progress / 50f
+                binding.wheelView.scaleX = scale
+                binding.wheelView.scaleY = scale
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+        })
+
     }
 }
